1fa91c4a889f8eacb04ba385ffcf0b829e7f595b
Will Hawkins <willhawkins@Wills-Mac-mini.local>
2025-10-07 13:10:09 +0100

    Fix: spelling error in email configuration settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -132,7 +132,7 @@ if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = 'stmp.gmail.com'
+    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
     EMAIL_HOST_USER = 'hawkinswill02@gmail.com'
